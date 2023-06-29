c504dadd44b1b31293632dba29fc385b011890b5
elenasegade <elena.sm.99@gmail.com>
2023-06-29 18:14:57 +0200

    Cos cambios no dirver-install

--- Dockerfiles/driver-builder/driver-install.sh
+++ Dockerfiles/driver-builder/driver-install.sh
@@ -5,3 +5,32 @@
 	echo 'exec ${IGNIS_HOME}/backend/jre/bin/java -cp "${IGNIS_HOME}/lib/java/*" org.ignis.backend.Main "$@"'; \
 } > ${IGNIS_HOME}/bin/ignis-backend
 chmod +x ${IGNIS_HOME}/bin/ignis-backend
+
+{ \
+	echo '#!/bin/bash'; \
+	echo 'if [ -z "$1" ]; then'
+    echo '  echo "Insert the job directory to restore"'
+    echo '  exit 1'
+	echo 'fi'
+	echo 'images_dir=$1'
+	echo 'sockets_file="$images_dir/sockets.txt"'
+	echo 'command=" criu restore -v4 --images-dir $images_dir --shell-job --tcp-established -x"'
+	echo 'for socket in $(cat "$sockets_file"); do'
+	echo '   command+=" $socket"'
+	echo 'done'
+	echo '$command'
+} > ${IGNIS_HOME}/bin/ignis-restore
+chmod +x ${IGNIS_HOME}/bin/ignis-restore
+
+export DEBIAN_FRONTEND=noninteractive
+apt update
+apt -y --no-install-recommends install python3.8 python3.8-distutils python3-numpy
+apt -y install libprotobuf-dev libprotobuf-c-dev protobuf-c-compiler protobuf-compiler python3-protobuf
+apt -y install libbsd-dev pkg-config libgnutls28-dev libnftables-dev asciidoc git build-essential libnet-dev libnl-3-dev libcap-dev
+wget https://download.openvz.org/criu/criu-3.15.tar.bz2
+tar -xjf criu-3.15.tar.bz2
+cd criu-3.15
+make install
+cd ..
+apt -y install iptables
+apt -y install lsof
--- backend/src/main/java/org/ignis/backend/cluster/tasks/executor/ICacheTask.java
+++ backend/src/main/java/org/ignis/backend/cluster/tasks/executor/ICacheTask.java
@@ -19,11 +19,13 @@ package org.ignis.backend.cluster.tasks.executor;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.apache.thrift.TException;
 import org.ignis.backend.cluster.IExecutor;
@@ -32,17 +34,8 @@ import org.ignis.backend.cluster.tasks.ICache;
 import org.ignis.backend.cluster.tasks.ICache.Level;
 import org.ignis.backend.exception.IExecutorExceptionWrapper;
 import org.ignis.backend.exception.IgnisException;
-import org.ignis.properties.IKeys;
 import org.ignis.rpc.IExecutorException;
 import org.slf4j.LoggerFactory;
-
-import java.lang.Thread;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
 /**
  * @author César Pomar
  */
@@ -52,15 +45,15 @@ public class ICacheTask extends IExecutorTask {
 
     private final ICache cache;
 
-    private final String directorioCriu;
+    private final String directoryCriu;
 
     public ICacheTask(String name, IExecutor executor, ICache cache) {
         super(name, executor);
         this.cache = cache;
-        String rutaCarpeta=executor.getContainer().getProperties().getProperty("ignis.dfs.home") + "/" 
+        String directory=executor.getContainer().getProperties().getProperty("ignis.dfs.home") + "/" 
                 + executor.getContainer().getProperties().getProperty("ignis.job.name")+"/checkpointing";
-        Path path = Paths.get(rutaCarpeta);
-        this.directorioCriu = rutaCarpeta;
+        Path path = Paths.get(directory);
+        this.directoryCriu = directory;
         try {
                 Files.createDirectories(path);
             } catch (IOException e) {
@@ -68,50 +61,49 @@ public class ICacheTask extends IExecutorTask {
             }
     }
     /*Obtiene el pid del proceso padre*/
-    private Long ontenerPidPadre(){
+    private Long getPidParent(){
         ProcessHandle currentProcess = ProcessHandle.current();
         Long pid = currentProcess.parent().get().pid();
-        LOGGER.error("Pid del padre "+ " " + pid);
         
         return pid;
     }
-    private void escribirSockets(String socket, BufferedWriter bw) throws IOException{
-        bw.write(socket+" ");
-        System.out.println(socket+" ");
+    private void writeSockets(String socket, String fd,BufferedWriter bw) throws IOException{
+        bw.write(" --inherit-fd fd["+fd+"]:socket:["+socket+"]");
     }
     /*Función que recoje todos los sockets para realizar el checkpointing*/
-    private String obtenerSockets(String pid) throws IOException{
+    private String getSockets(String pid) throws IOException{
         Process p=Runtime.getRuntime().exec("lsof -U +c 0");
-        String comandoDump = "nohup criu dump -D " + this.directorioCriu + " -t " + pid + " --tcp-established --leave-running --shell-job --ext-unix-sk";
-        BufferedWriter bw=new BufferedWriter(new FileWriter(directorioCriu+"/sockets.txt"));
+        String commandDump = "nohup criu dump -v4 -D " + this.directoryCriu + " -t " + pid + " --tcp-established --leave-running --shell-job --ext-unix-sk";
+        BufferedWriter bw=new BufferedWriter(new FileWriter(directoryCriu+"/sockets.txt"));
         try(BufferedReader br= new BufferedReader (new InputStreamReader(p.getInputStream()))){
-            String salida;
+            String out;
             br.readLine();
-            while ((salida = br.readLine()) != null) {
+            while ((out = br.readLine()) != null) {
                 // Parsear la línea de salida de lsof para obtener el número del socket UNIX
-                String[] letras = salida.split("\\s+");
-                String socket = letras[7];
-                comandoDump = comandoDump + " --external unix[" + socket + "]";
-                escribirSockets(socket, bw);
+                String[] words = out.split("\\s+");
+                String socket = words[7];
+                String fd = words[3];
+                commandDump = commandDump + " --external unix[" + socket + "]";
+                writeSockets(socket, fd, bw);
             }
         }
 
-        comandoDump = comandoDump + " > /tmp/criu.log &";
-        System.err.println(comandoDump);
+        commandDump = commandDump + " &";
+        LOGGER.info(commandDump);
         bw.close();
-        return comandoDump;
+        return commandDump;
     }
         
     /*Función que realiza el checkpointing del proceso*/
-    private void realizarCheckpointing(String pid){
+    private void doCheckpointing(String pid){
         try {
             //Se define la ruta donde se guardará el checkpointing
-            String cDumpp=obtenerSockets(pid);
+            String cDumpp=getSockets(pid);
 
             //Se realiza el checkpointing en la ruta especificada y el pid del proceso
           
             Process pDump = Runtime.getRuntime().exec(new String[] { "bash", "-c", cDumpp});
-            File f= new File(directorioCriu);
+            File f= new File(directoryCriu);
             
             //Para ver la salida del comando
             while(f.list().length==0){
@@ -119,21 +111,16 @@ public class ICacheTask extends IExecutorTask {
                     Thread.sleep(20);
                 }
                 catch (Exception e) {
-                    System.out.println(e);
+                    LOGGER.error("CRIU error: ",e);
                 }
-            }
-            InputStream stdout= new FileInputStream("/tmp/criu.log");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(stdout));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.err.println(line);
             }
 
             // Leer la salida de error del proceso
+            String line;
             InputStream stderr = pDump.getErrorStream();
             BufferedReader errorReader = new BufferedReader(new InputStreamReader(stderr));
             while ((line = errorReader.readLine()) != null) {
-                System.err.println(line);
+                LOGGER.info(line);
             }
 
             
@@ -154,9 +141,8 @@ public class ICacheTask extends IExecutorTask {
                 executor.getCacheContextModule().cache(cache.getId(), (byte) ICache.Level.NO_CACHE.getInt());
             }
             if(cache.getNextLevel().equals(ICache.Level.CHECKPOINT)){
-                System.err.print("Entrou no if");
-                Long pid = ontenerPidPadre();
-                realizarCheckpointing(pid.toString());
+                Long pid = getPidParent();
+                doCheckpointing(pid.toString());
                 executor.getCacheContextModule().cache(cache.getId(), (byte) Level.DISK.getInt());
             }else{
                 executor.getCacheContextModule().cache(cache.getId(), (byte) cache.getNextLevel().getInt());
